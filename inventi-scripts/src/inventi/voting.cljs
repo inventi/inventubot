52a6c1a1fd26440f82f87089721a1372ef6f47d6
Lukas Taskunas <lukas@inventi.lt>
2016-03-10 12:11:50 +0200

    Disallow negative numbers in the roll command

--- inventi-scripts/src/inventi/voting.cljs
+++ inventi-scripts/src/inventi/voting.cljs
@@ -3,6 +3,7 @@
 
 (defn roll [res]
   (let [value (string->int (second res.match))
-       max-val (if (integer? value) value 100)]
+       max-val (if (and (integer? value)
+                       (> value 0)) value 100)]
     (.send res (str "Rolling a number [1-" max-val "]: " (+ 1 (rand-int max-val))))))
 
