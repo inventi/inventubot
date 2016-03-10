eaeb109322dd4fff86d5a1870449566cff007235
Lukas Taskunas <lukas@inventi.lt>
2016-03-10 12:12:16 +0200

    Add command that generates random UUID

--- inventi-scripts/src/inventi/hubot.cljs
+++ inventi-scripts/src/inventi/hubot.cljs
@@ -1,12 +1,14 @@
 (ns inventi.hubot
   (:require [inventi.books :as books]
             [inventi.contacts :as contacts]
-            [inventi.voting :as voting]))
+            [inventi.voting :as voting]
+            [inventi.uuid :as uuid]))
 
 (defn init-scripts [bot]
   (.respond bot "/add book (.*)/i" books/respond-add-book)
   (.respond bot "/contacts/i" contacts/respond-contacts)
-  (.respond bot "/roll ?(.*)/i" voting/roll))
+  (.respond bot "/roll ?(.*)/i" voting/roll)
+  (.respond bot "/uuid/i" uuid/random))
 
 (defn -main []
   (println "inventi-scripts initialized"))
